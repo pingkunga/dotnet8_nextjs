e3a72ca2046a7fbddea6b90d100a47400629e007
Chatri Ngambenchawong <pingkunga@gmail.com>
2024-04-06 14:30:20 +0700

    feat: add adm panel

--- test/next-tailwind-approunter/src/components/front/Navbar.tsx
+++ test/next-tailwind-approunter/src/components/front/Navbar.tsx
@@ -2,13 +2,14 @@
 
 import Link from 'next/link'
 import React from 'react'
-
+import { usePathname } from 'next/navigation'
 
 //class not support in jsx 
 //solution 
 // drag html file to jsx file
 // in vscode right click and convert html to jsx
 export default function Navbar() {
+  const currentPath = usePathname()
   return (
    <header className="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white border-b border-gray-200 text-sm py-3 sm:py-0 dark:bg-gray-800 dark:border-gray-700">
     <nav className="relative max-w-7xl w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8" aria-label="Global">
@@ -31,19 +32,31 @@ export default function Navbar() {
         <div id="navbar-collapse-with-animation" className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:block">
         <div className="flex flex-col gap-y-4 gap-x-0 mt-5 sm:flex-row sm:items-center sm:justify-end sm:gap-y-0 sm:gap-x-7 sm:mt-0 sm:ps-7">
             {/* a = link in next js*/ }
-            <Link className="font-medium text-blue-600 sm:py-6 dark:text-blue-500" 
+            <Link className={currentPath ==="/home" 
+                                ? "font-medium text-blue-600 sm:py-6 dark:text-blue-500" 
+                                :"font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500"
+                            }
                   href="/home">
                   Home
             </Link>
-            <Link className="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500" 
+            <Link className={currentPath ==="/about" 
+                                ? "font-medium text-blue-600 sm:py-6 dark:text-blue-500" 
+                                :"font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500"
+                            }
                   href="/about">
                   About
             </Link>
-            <Link className="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500" 
+            <Link className={currentPath ==="/blog" 
+                                ? "font-medium text-blue-600 sm:py-6 dark:text-blue-500" 
+                                :"font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500"
+                            }
                   href="/blog">
                   Blog
             </Link>
-            <Link className="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500" 
+            <Link className={currentPath ==="/contract" 
+                                ? "font-medium text-blue-600 sm:py-6 dark:text-blue-500" 
+                                :"font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500"
+                            }
                   href="/contract">
                   Contract
             </Link>
