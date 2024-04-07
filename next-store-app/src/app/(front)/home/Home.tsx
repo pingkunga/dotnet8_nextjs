29309108d0284bccc47d9ae3507b3a20bca0ab10
Chatri Ngambenchawong <pingkunga@gmail.com>
2024-04-07 22:10:44 +0700

    feat: add banner / slide and related resource

--- next-store-app/src/app/(front)/home/Home.tsx
+++ next-store-app/src/app/(front)/home/Home.tsx
@@ -1,6 +1,10 @@
+'use client'
+
 import { Button, Typography } from '@mui/material'
 import React from 'react'
 import { AddCircleOutline as AddCirIcon } from '@mui/icons-material'
+import Banner from '@/app/components/front/banner/Banner'
+import BannerSlider from '@/app/components/front/banner/BannerSlider'
 
 
 type Props = {}
@@ -8,7 +12,7 @@ type Props = {}
 export default function HomePage({}: Props) {
   return (
     <>
-    <Typography variant="h1">Hello World</Typography>
+    {/* <Typography variant="h1">Hello World</Typography>
 
     <Button variant="contained" color="primary"> Test Button </Button>
 
@@ -20,7 +24,10 @@ export default function HomePage({}: Props) {
       <AddCirIcon color="info" />
       <AddCirIcon color="success" />
 
-    </div>
+    </div> */}
+    <Banner />
+    <BannerSlider />  
+      
   </>
   )
 }
