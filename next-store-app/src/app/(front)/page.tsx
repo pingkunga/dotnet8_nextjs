49dd162395cb2579372d96292865cf1e9ea98900
Chatri Ngambenchawong <pingkunga@gmail.com>
2024-04-07 10:33:15 +0700

    feat: add 404 + meta data seo

---
+++ next-store-app/src/app/(front)/page.tsx
@@ -0,0 +1,27 @@
+import { Add } from '@mui/icons-material'
+import { Button, Typography } from '@mui/material'
+import React from 'react'
+
+import { AddCircleOutline as AddCirIcon } from '@mui/icons-material'
+
+type Props = {}
+
+export default function Home({}: Props) {
+  return (
+    <>
+      <Typography variant="h1">Hello World</Typography>
+
+      <Button variant="contained" color="primary"> Test Button </Button>
+
+      <div>
+        <AddCirIcon color="primary" />
+        <AddCirIcon color="secondary" />
+        <AddCirIcon color="error" />
+        <AddCirIcon color="warning" />
+        <AddCirIcon color="info" />
+        <AddCirIcon color="success" />
+
+      </div>
+    </>
+  )
+}
