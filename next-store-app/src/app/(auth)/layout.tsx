e10f3cfe336167742575f77403413593d4a0b66c
Chatri Ngambenchawong <pingkunga@gmail.com>
2024-04-09 21:01:12 +0700

    fixbug: remove nest html tag when render

--- next-store-app/src/app/(auth)/layout.tsx
+++ next-store-app/src/app/(auth)/layout.tsx
@@ -1,19 +1,11 @@
-import React from 'react'
-import Header from '@/app/components/front/header/Header';
-import Footer from '@/app/components/front/footer/Footer';
-
 export default function AuthLayout({
-        children,
-      }: Readonly<{
-        children: React.ReactNode;
-      }>) {
+    children,
+  }: Readonly<{
+    children: React.ReactNode
+  }>) {
   return (
-    <html lang="en">
-        <body>
-            <Header />
-            {children}
-            <Footer />
-        </body>
-    </html>
+    <>  
+      {children}
+    </>
   )
 }
