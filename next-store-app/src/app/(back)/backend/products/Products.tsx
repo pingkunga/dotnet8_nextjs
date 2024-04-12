041c9e046d190d5afabd47630c8023ec412855f6
Chatri Ngambenchawong <pingkunga@gmail.com>
2024-04-12 07:51:57 +0700

    feat: improve ux add icon CRUD

--- next-store-app/src/app/(back)/backend/products/Products.tsx
+++ next-store-app/src/app/(back)/backend/products/Products.tsx
@@ -1,60 +1,62 @@
-"use client"
+"use client";
 
 //React
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 // Material-UI
-import { Box
-       , Table
-       , TableBody
-       , TableCell
-       , TableContainer
-       , TableHead
-       , TableRow
-       , Paper, 
-       Card,
-       CardContent,
-       Stack,
-       Typography,
-       Button} from '@mui/material'
+import {
+  Box,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Card,
+  CardContent,
+  Stack,
+  Typography,
+  Button,
+} from "@mui/material";
 
 // Custom Components
-import DashboardCard from '@/app/components/back/shared/DashboardCard'
+import DashboardCard from "@/app/components/back/shared/DashboardCard";
 
 // Business Logic
-import { getAllProducts } from '@/app/services/actions/productAction'
+import { getAllProducts } from "@/app/services/actions/productAction";
+import { IconEdit, IconEye, IconPlus, IconTrash } from "@tabler/icons-react";
 
 type Product = {
-  product_id: number
-  category_name: string
-  product_name: string
-  unit_price: number
-  product_picture: string
-  unit_in_stock: number
-  created_date: string
-  modified_date: string
-}
+  product_id: number;
+  category_name: string;
+  product_name: string;
+  unit_price: number;
+  product_picture: string;
+  unit_in_stock: number;
+  created_date: string;
+  modified_date: string;
+};
 
-type Props = {}
+type Props = {};
 
-export default function ProductsPage({ }: Props) {
-
-  const [products, setProducts] = useState([])
+export default function ProductsPage({}: Props) {
+  const [products, setProducts] = useState([]);
 
   const fetchProducts = async () => {
     try {
-      const response = await getAllProducts()
-      setProducts(response)
+      const response = await getAllProducts();
+      setProducts(response);
     } catch (error) {
-      console.error('An error occurred while fetching products:', error)
+      console.error("An error occurred while fetching products:", error);
     }
-  }
+  };
 
   useEffect(() => {
-    fetchProducts()
-  }, [])
+    fetchProducts();
+  }, []);
 
-  console.log(products)
+  console.log(products);
 
   return (
     <>
@@ -70,50 +72,114 @@ export default function ProductsPage({ }: Props) {
             alignItems={"center"}
           >
             <Typography variant="h5">Products</Typography>
-            <Button
-              variant="contained"
-              color="primary"
-            >
-              Add Product
+            <Button variant="contained" color="primary">
+              <IconPlus size={16} /> &nbsp;Add Product
             </Button>
           </Stack>
         </CardContent>
-      {/* <Box mt={2}>
-        <DashboardCard title="Products">
-          
-          <TableContainer component={Paper}> */}
-              <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                  <TableHead>
-                      <TableRow>
-                          <TableCell>ID</TableCell>
-                          <TableCell>Product</TableCell>
-                          <TableCell>Category</TableCell>
-                          <TableCell>Price</TableCell>
-                          <TableCell>Unit</TableCell>
-                      </TableRow>
-                  </TableHead>
-                  <TableBody>
-                      {products.map((product: Product) => (
-                          <TableRow
-                              key={product.product_id}
-                              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                          >
-                              <TableCell component="th" scope="product">
-                                  {product.product_id}
-                              </TableCell>
-                              <TableCell>{product.product_name}</TableCell>
-                              <TableCell>{product.category_name}</TableCell>
-                              <TableCell>{product.unit_price}</TableCell>
-                              <TableCell>{product.unit_in_stock}</TableCell>
-                          </TableRow>
-                      ))}
-                  </TableBody>
-              </Table>
-          {/* </TableContainer>
-
-        </DashboardCard>
-      </Box> */}
+        <Box sx={{ overflow: "auto", width: { sm: "auto" } }}>
+          <Table
+            aria-label="products"
+            sx={{
+              whiteSpace: "nowrap",
+              mt: 2,
+            }}
+          >
+            <TableHead>
+              <TableRow>
+                <TableCell>
+                  <Typography variant="subtitle2" fontWeight={600}>
+                    ID
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="subtitle2" fontWeight={600}>
+                    Picture
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="subtitle2" fontWeight={600}>
+                    Product
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="subtitle2" fontWeight={600}>
+                    Category
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="subtitle2" fontWeight={600}>
+                    Price
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="subtitle2" fontWeight={600}>
+                    Unit
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="subtitle2" fontWeight={600}>
+                    Created
+                  </Typography>
+                </TableCell>
+                <TableCell sx={{ width: "100px" }}>
+                  <Typography variant="subtitle2" fontWeight={600}>
+                    Manage
+                  </Typography>
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {products.map((product: Product) => (
+                <TableRow
+                  key={product.product_id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="product">
+                    {product.product_id}
+                  </TableCell>
+                  <TableCell>
+                    <img
+                      src={`${process.env.NEXT_PUBLIC_BASE_IMAGE_URL_API}/${product.product_picture}`}
+                      alt={product.product_name}
+                      style={{ width: "50px" }}
+                    />
+                  </TableCell>
+                  <TableCell>{product.product_name}</TableCell>
+                  <TableCell>{product.category_name}</TableCell>
+                  <TableCell>{product.unit_price}</TableCell>
+                  <TableCell>{product.unit_in_stock}</TableCell>
+                  <TableCell>{product.created_date}</TableCell>
+                  <TableCell>
+                    {/* Button View, Edit and Delete with Icon */}
+                    <Button
+                      variant="contained"
+                      color="info"
+                      sx={{ mr: 1, minWidth: "30px" }}
+                    >
+                      <IconEye size={16} />
+                    </Button>
+                    <Button
+                      variant="contained"
+                      color="warning"
+                      sx={{ mr: 1, minWidth: "30px" }}
+                    >
+                      <IconEdit size={16} />
+                    </Button>
+                    <Button
+                      variant="contained"
+                      color="error"
+                      sx={{ mr: 1, minWidth: "30px" }}
+                    >
+                      <IconTrash size={16} />
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Box>
       </Card>
     </>
-  )
+  );
 }
